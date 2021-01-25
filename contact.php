bd9ec8cbdee75298b407a1f3c8c6464092ff6a7e
Risper Akinyi <email@example.com>
2021-01-25 11:27:45 +0300

    all

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@ $error_message = "";
 if(isset($_POST['contact'])) {
  
     
-    $email_to = "sulabknight@gmail.com";
+    $email_to = "wicliferono@gmail.com";
     $email_subject = "Contact Form";
  
     function died($error) {
